fc77a62cc82d2bc4125454b611cae9095d2cd109
qing <letjinxgo@gmail.com>
2025-01-11 22:47:32 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+CHART_REPO_NAME=$1
+CHART_URL=$2
+IMAGE_NAME=$3
+RELEASE_NAME=$4
+NAMESPACE=$5
 helm repo add ${CHART_REPO_NAME} ${CHART_URL}
 helm repo update
 helm upgrade \
---install ${params.IMAGE_NAME}${params.RELEASE_NAME} ${CHART_REPO_NAME}/${params.IMAGE_NAME} \
---namespace ${env.NAMESPACE}
+--install ${IMAGE_NAME}${RELEASE_NAME} ${CHART_REPO_NAME}/${IMAGE_NAME} \
+--namespace ${NAMESPACE}
